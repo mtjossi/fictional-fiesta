e0bedfc4e60f1c5fc53074a0e0539bc74d2d9adb
mtjossi <45386128+mtjossi@users.noreply.github.com>
2025-03-09 23:54:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def get_latest2():
     df2_filled['raw_number2'] = 0
 
     for i, v in tqdm(enumerate(df2_filled['ISIN']), total=df2_filled.shape[0]):
+        print(f"Now trying {v}")
         if v not in ['UZ7011030002', 'UZ7028090007', 'UZ7035340007', 'UZ7004770002', 'UZ7021490006', 
         'UZ7016990002', 'UZ7030360000', 'UZ7032740001', 'UZ7016550004', 'UZ7016530006','UZ7026620003',
         'UZ7017850007']:
@@ -30,7 +31,7 @@ def get_latest2():
             numbers = soup.find_all(class_="trd-price")
             number = numbers[-1].text
             dates = soup.find_all(class_="text-left")
-            date = pd.to_datetime(dates[-1].text.strip(), format="%d.%m.%Y").date().isoformat()
+            date = pd.to_datetime(dates[-1].text.strip(), format="%Y.%m.%d").date().isoformat()
             table = soup.find_all("table", attrs={"class":"table centered-table table-bordered"})
             table_body = table[3].find("tbody")
             table_row = table_body.find_all("tr")[0]
@@ -70,15 +71,16 @@ def get_latest2():
 
     for j, k in tqdm(enumerate(df2_filled['raw_number']), total=df2_filled.shape[0]):
         if k != 0:
-            price = float(k.strip().replace(',','.').replace(' ',''))
-            df2_filled.loc[j, 'PRICE'] = price
-            try:
-                price2 = float(df2_filled.loc[j, 'raw_number2'].strip().replace(',','.').replace(' ',''))
-                df2_filled.loc[j, 'PRICE2'] = price2
-            except:
-                df2_filled.loc[j, 'PRICE2'] = float(df2_filled.loc[j, 'raw_number2'])
-            date2 = pd.to_datetime(df2_filled.loc[j, "date_raw"], format="%d.%m.%Y").date().isoformat()
-            df2_filled.loc[j, 'date2'] = date2
+            if '.' not in k:
+                price = float(k.strip().replace(',','.').replace(' ',''))
+                df2_filled.loc[j, 'PRICE'] = price
+                try:
+                    price2 = float(df2_filled.loc[j, 'raw_number2'].strip().replace(',','.').replace(' ',''))
+                    df2_filled.loc[j, 'PRICE2'] = price2
+                except:
+                    df2_filled.loc[j, 'PRICE2'] = float(df2_filled.loc[j, 'raw_number2'])
+                date2 = pd.to_datetime(df2_filled.loc[j, "date_raw"], format="%Y-%m-%d").date().isoformat()
+                df2_filled.loc[j, 'date2'] = date2
   
 
     df2_filled[''] = ''
@@ -108,6 +110,7 @@ def get_latest3():
         'UZ7017850007']:
             
             START_URL = "https://www.uzse.uz/isu_infos/STK?isu_cd="
+            print(v)
             URL = f"{START_URL}{v}"
             r = requests.get(URL, verify=False)
             sleep(1)
@@ -115,7 +118,7 @@ def get_latest3():
             numbers = soup.find_all(class_="trd-price")
             number = numbers[-1].text
             dates = soup.find_all(class_="text-left")
-            date = pd.to_datetime(dates[-1].text.strip(), format="%d.%m.%Y").date().isoformat()
+            date = pd.to_datetime(dates[-1].text.strip(), format="%Y.%m.%d").date().isoformat()
             table = soup.find_all("table", attrs={"class":"table centered-table table-bordered"})
             table_body = table[3].find("tbody")
             table_row = table_body.find_all("tr")[0]
@@ -129,41 +132,20 @@ def get_latest3():
 
         else:
             pass
-            # # # START_URL = "http://elsissavdo.uz/results?ResultsSearch%5Btrtime%5D=&ResultsSearch%5Bstock%5D="
-            # URL2 = f"http://otcmarket.uz/results?ResultsSearch%5Btrtime%5D=&ResultsSearch%5Bstock%5D={v}&ResultsSearch%5Bemitent%5D=&ResultsSearch%5Bquantity%5D=&ResultsSearch%5Bprice%5D="
-            # # URL2 = f"{START_URL}{v}"
-            # temp_df = pd.read_html(URL2)[0]
-            # temp_df = temp_df.dropna().iloc[0,[2,6]]
-            # if temp_df[1] != 'No results found.':
-            #     price = temp_df[1]
-            #     try:
-            #         price = float(price.replace(' ',''))
-            #     except:
-            #         price = float(price)
-            #     date = temp_df[0]
-            #     date = pd.to_datetime(date.split(' ')[0], format="%d-%m-%Y").date().isoformat()
-            # else:
-            #     price = 0
-            #     date = 'N/A'
-
-
-            # df2_filled.loc[i, 'PRICE2'] = price
-            # df2_filled.loc[i, 'date2'] = date
-            # df2_filled.loc[i, 'PRICE'] = price
-            # df2_filled.loc[i, 'price as of'] = date
-
+        
 
     for j, k in tqdm(enumerate(df2_filled['raw_number']), total=df2_filled.shape[0]):
         if k != 0:
-            price = float(k.strip().replace(',','.').replace(' ',''))
-            df2_filled.loc[j, 'PRICE'] = price
-            try:
-                price2 = float(df2_filled.loc[j, 'raw_number2'].strip().replace(',','.').replace(' ',''))
-                df2_filled.loc[j, 'PRICE2'] = price2
-            except:
-                df2_filled.loc[j, 'PRICE2'] = float(df2_filled.loc[j, 'raw_number2'])
-            date2 = pd.to_datetime(df2_filled.loc[j, "date_raw"], format="%d.%m.%Y").date().isoformat()
-            df2_filled.loc[j, 'date2'] = date2
+            if '.' not in k:
+                price = float(k.strip().replace(',','.').replace(' ',''))
+                df2_filled.loc[j, 'PRICE'] = price
+                try:
+                    price2 = float(df2_filled.loc[j, 'raw_number2'].strip().replace(',','.').replace(' ',''))
+                    df2_filled.loc[j, 'PRICE2'] = price2
+                except:
+                    df2_filled.loc[j, 'PRICE2'] = float(df2_filled.loc[j, 'raw_number2'])
+                date2 = pd.to_datetime(df2_filled.loc[j, "date_raw"], format="%Y.%m.%d").date().isoformat()
+                df2_filled.loc[j, 'date2'] = date2
   
 
     df2_filled[''] = ''
